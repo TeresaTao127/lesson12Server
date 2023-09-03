ef5667b61438fde72a98c6c80a680f4b79c43603
TeresaTao127 <taoleiluck2020@gmail.com>
2023-09-03 16:36:53 +0100

    lesson12 task2

---
+++ src/main/java/lesson12Task2/EchoServerApplication.java
@@ -0,0 +1,12 @@
+package lesson12Task2;
+
+import com.futurecollars.serverdemo.ServerdemoApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EchoServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerdemoApplication.class, args);
+    }
+}
